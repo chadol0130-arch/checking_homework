fbdcfef3193a800c6a60228c4a607a0efff6129e
chadol0130-arch <chadol0130@icloud.com>
2026-01-26 16:24:12 +0900

    firebase

--- static/app.js
+++ static/app.js
@@ -1,3 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyChYb-nd-jhtkrCub8thxAU1xQHrQ2Zk-A",
+  authDomain: "checking-homework-5647d.firebaseapp.com",
+  projectId: "checking-homework-5647d",
+  storageBucket: "checking-homework-5647d.firebasestorage.app",
+  messagingSenderId: "220286491925",
+  appId: "1:220286491925:web:1526b0a7b167e9e4ce1d1c",
+  measurementId: "G-24VJYH8SEH"
+  databaseURL: "https://checking-homework-5647d-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
+
 const form = document.getElementById("upload-form");
 const resultBox = document.getElementById("result");
 const levelText = document.getElementById("level");
